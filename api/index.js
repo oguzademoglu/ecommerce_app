e747724b31d88f469bb81a5bd0682a77f0841efa
oguzademoglu <oguzademoglu1@gmail.com>
2023-06-26 11:51:48 +0300

    mongoDb connection enabled

--- api/index.js
+++ api/index.js
@@ -1,14 +1,30 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 
 app.use(express.json());
 
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL, 
+        console.log('Connected to MongoDB...')
+        );        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+mongoose.connection.on("disconnected", () => {
+    console.log("mongoDb disconnected!");
+});
+
 app.get('/', (req,res) => {
     res.send("Hello ");
-})
+});
 
 
 app.listen(8800, () => {
-    console.log('app listening on port 3001');
+    connect();
+    console.log('app listening on port 8800');
 });
